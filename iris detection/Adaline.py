d7ec5423565cca8bec65319c999f4162ed83790e
souhail louzi <117845917+xfightervx@users.noreply.github.com>
2024-03-22 19:07:06 +0100

    add an Adaline approach

---
+++ iris detection/Adaline.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import pandas as pd
+
+class Adaline:
+    """ADAptive LInear NEuron classifier.
+
+    Parameters:
+
+    eta : float
+        Learning rate (between 0.0 and 1.0) 
+    n_iter : int
+        Passes over the training dataset
+    random_state : int.
+        Random number generator seed for random weight intialization.
+
+
+    Attributes:
+
+    w_ : id-array
+        Weights after fitting
+    errors_ : list
+        Number of misclassifications in every epoch.
+     
+      
+        """
+    
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, Y):
+        """ Fit training data.
+
+        Parameters:
+
+        X : (arrau), shape = [n_samples, n_features]
+            Training vectors, where n_samples is the number of samples and n_featurs is the number of features.
+        Y : array, shape = [n_samples]
+            Target values.
+
+        Returns:
+
+
+
+        self : object
+           """
+        
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0 , scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for _ in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (Y - output)
+            self.w_[1:] +=self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum()/2.0
+            self.cost_.append(cost)
+        return self
+    def net_input(self, X):
+        """ Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0] 
+
+    def activation(self, X):
+        return X
+    def predict(self, X):
+        """Return class Label after unit step"""
+        return np.where(self.net_input(X) >= 0.0 , 1 , -1)
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    markers = ('s','x','o','^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    x1_min , x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8,c=colors[idx],marker=markers[idx], label = cl, edgecolors='black')
+    
+
+df = pd.read_csv("iris detection\data\iris.data", header=None)
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1 , 1)
+X = df.iloc[0:100, [0,2]].values
+
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+ada = Adaline(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
